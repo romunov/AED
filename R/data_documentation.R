d14a573cf375b589080390842def8771ff7539b4
romunov <romunov@gmail.com>
2015-02-17 23:18:02 +0100

    added Seals data

---
+++ R/data_documentation.R
@@ -0,0 +1,15 @@
+#' Common seal (\emph{Phoca vitulina} L.) data comes from Cronin et al. 2007, where between April 2003 and November 2005, regular
+#' standardised haul-out count survey of Bantry Bay and Kenmare River was carried out by boat. Counts of seals at each haul-out
+#' site was carried out independently and simultaneously by two observers, initially from 200 m and progressively closer. Read
+#' more about the data in Zuur et al. 2007 in chapter 23 (page 503) or in above mentioned Cronin et al. 2007 paper.
+#' 
+#' @name Seals
+#' @title Seals data
+#' @description Common seal (\emph{Phoca vitulina} L.) data comes from Cronin et al. 2007.
+#' @docType data
+#' @usage data(Seals)
+#' @source Unknown
+#' @references Cronin et al. 2007. An assessment of population size and distribution of hrabour seals in the Republic ofIreland during the moult season in August 2003. Journal of zoology, 273: 131-139
+#' @references Zuur et al. 2009. Mixed effects models and extensions in ecology with R. Springer. ISBN: 978-0-387-87457-9.
+NULL
+
