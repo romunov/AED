2cf8ea7ffc2e642cc4aa2b921e1bbca47e7a6683
romunov <romunov@gmail.com>
2015-02-18 18:49:56 +0100

    added nereis data

---
+++ R/nereis.R
@@ -0,0 +1,9 @@
+#' Data of deposit-feeding \emph{Nereis (Hediste) diversicolor} comes from Ieno et al. (2006). Data was collected to model nutrient concentration.
+#' 
+#' @name Nereis
+#' @title Nereis data
+#' @description Data of deposit-feeding \emph{Nereis (Hediste) diversicolor} from Ieno et al. (2006).
+#' @docType data
+#' @usage data(Nereis)
+#' @references Ieno et al. 2006. Distinguishing between the effects of infaunal species richness, identity and density in the marine benthos. Marine ecology progress series, 311: 263-271.
+NULL 
