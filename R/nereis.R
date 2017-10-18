aeae81930adf50a017f543b40ddc7c3226df292a
jsta <stachel2@msu.edu>
2017-10-18 08:52:14 -0400

    fill out DESCRIPTION, pass devtools::check()

--- R/nereis.R
+++ R/nereis.R
@@ -4,6 +4,6 @@
 #' @title Nereis data
 #' @description Data of deposit-feeding \emph{Nereis (Hediste) diversicolor} from Ieno et al. (2006).
 #' @docType data
-#' @usage data(Nereis)
+#' @usage Nereis
 #' @references Ieno et al. 2006. Distinguishing between the effects of infaunal species richness, identity and density in the marine benthos. Marine ecology progress series, 311: 263-271.
 NULL 
