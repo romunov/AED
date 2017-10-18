1641198d011daff96b6b9ed86c9b8df03383f0d2
jsta <stachel2@msu.edu>
2017-10-18 08:50:44 -0400

    add RIKZ dataset

---
+++ R/RIKZ.R
@@ -0,0 +1,12 @@
+#' Marine benthic data from nine inter-tidal areas along the Dutch coast. The 
+#' data were collected by the Dutch institute RIKZ in the summer of 2002. In 
+#' each inter-tidal area (denoted by ‘beach’), five samples were taken, and the 
+#' macro-fauna and abiotic variables were measured.
+#' 
+#' @name RIKZ
+#' @title Marine benthic data
+#' @description Marine benthic data comes from Zuur et al. (2007).
+#' @docType data
+#' @usage RIKZ
+#' @references Zuur, A., Ieno, E.N. and Smith, G.M., 2007. Analyzing ecological data. Springer Science & Business Media.
+NULL
