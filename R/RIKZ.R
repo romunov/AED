5b18ccc312eed240a02c03dcd4db67af944db296
jsta <stachel2@msu.edu>
2017-10-18 09:54:46 -0400

    add RIKZ variable descriptions

--- R/RIKZ.R
+++ R/RIKZ.R
@@ -6,6 +6,13 @@
 #' @name RIKZ
 #' @title Marine benthic data
 #' @description Marine benthic data comes from Zuur et al. (2007).
+#' @details \itemize{ 
+#'     \item Sample: sample number
+#'     \item Richness: species richness
+#'     \item Exposure: index composed of the surf zone, slope, grain size, and depth of anaerobic layer
+#'     \item NAP: height of sampling station compared to mean tidal level
+#'     \item Beach: beach identifier
+#'     }
 #' @docType data
 #' @usage RIKZ
 #' @references Zuur, A., Ieno, E.N. and Smith, G.M., 2007. Analyzing ecological data. Springer Science & Business Media.
