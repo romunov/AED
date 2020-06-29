ed9741368c17cffbae5f717defb03a47a3768d16
Roman Luštrik <roman.lustrik@biolitika.si>
2020-06-29 19:36:18 +0200

    Add wild boar Tb dataset

---
+++ R/wildboar_tb.R
@@ -0,0 +1,24 @@
+#' Vicente et al. (2006) analyzed the distribution on tuberculosis-like lesions in wild boar (\emph{Sus scrofa}) for
+#' potential importance of persistence of tuberculosis in south central Spain. Data came from harvested animals.
+#' Tuberculosis (Tb) is a zoonosis that can jump from non-human to human animal, which makes the disease interesting to
+#' study for its economical and public health impacts.
+#' 
+#' The dataset comes with variables:
+#' \itemize{
+#'     \item Tb: An integer denoting presence (1) or absence (0) of tuberculosis in an animal
+#'     \item sex: Sex of the animal harvested
+#'     \item age_class: Age class of the animal based on tooth eruption patterns. Boars less than 7 months old were
+#'     classified as piglets, boars between 7 and 12 months were classified as weaners, those between 12 and 24 months
+#'     as juveniles, and those more than 2 years old as adults.
+#'     \item length_ct: Head-body length of the animal in cm, where CT is Spanish abbreviation for \emph{cabeza-tronco}.
+#' }
+#' 
+#' @name WildBoarTb
+#' @title Wild boar tuberculosis-like lesions dataset
+#' @description Presence or absence of tuberculosis in wild boar (\emph{Sus scrofa}) from south central Spain
+#' @docType data
+#' @usage data("WildBoarTb")
+#' @references Vicente J, Höfle U, Garrido JM, Fernández-de-Mera IG, Juste R, Barralb M, Gortazar C. 2006. Wild boar
+#' and red deer display high prevalences of tuberculosis-like lesions in Spain. Veterinary research 37: 107-119.
+#' @references Zuur et al. 2009. Mixed effects models and extensions in ecology with R. Springer. ISBN: 978-0-387-87457-9.
+NULL
